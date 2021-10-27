029e658f1bc4de1da4858db36e71e51a1f086299
linam616 <90432299+linam616@users.noreply.github.com>
2021-10-27 03:47:35 -0500

    Revert "lina first update"

--- Project/rplannerg/src/App.js
+++ Project/rplannerg/src/App.js
@@ -1,110 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import ReactDOM from 'react-dom';
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org/"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          fuck you react
+          Learn React
         </a>
       </header>
     </div>
-    )
-  }
+  );
 }
-
-/*
-const Columns = () => 
-  <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
-    <div>To-Do's</div>
-    <div>Deadlines</div>
-    <div>Column 3</div>
-  </div>
-
-ReactDOM.render(<Columns />, document.getElementById("root"))
-*/
-
-//To-Do List with inputs
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
-      </div>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
-}
-
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    );
-  }
-}
-
-
 
 export default App;
-
-//target container is not a DOM element error
-
-ReactDOM.render(
-  <TodoApp />,
-  document.getElementById('todos-example')
-);
