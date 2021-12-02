1782b9f25a2d7e8894a541fb8a3d768189a81aa2
Lina <Linamei0616@gmail.com>
2021-12-02 00:34:54 -0600

    'added dropdown menu for course requirements

---
+++ Project/rplannerg/src/components/CourseRequirements.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+const CourseMain = styled("div")`
+  font-family: sans-serif;
+  background: #C2AFF0;
+  height: 100vh;
+`;
+
+const DropDownContainer = styled("div")`
+  width: 10.5em;
+  margin: 0 auto;
+`;
+
+const DropDownHeader = styled("div")`
+  margin-bottom: 0.8em;
+  padding: 0.4em 2em 0.4em 1em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  font-weight: 500;
+  font-size: 1.3rem;
+  color: #3faffa;
+  background: #F0F6F6;
+`;
+
+const DropDownListContainer = styled("div")``;
+
+const DropDownList = styled("ul")`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+const ListItem = styled("li")`
+  list-style: none;
+  margin-bottom: 0.8em;
+`;
+
+function CourseRequirements() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggling = () => setIsOpen(!isOpen);
+
+  return (
+    <CourseMain>
+      <h1>See your class requirements!</h1>
+      <DropDownContainer>
+        <DropDownHeader onClick={toggling}>Majors</DropDownHeader>
+        {isOpen && (
+          <DropDownListContainer>
+            <DropDownList>
+              <ListItem>Computer Science</ListItem>
+              <ListItem>Engineering</ListItem>
+              <ListItem>Eduction</ListItem>
+            </DropDownList>
+          </DropDownListContainer>
+        )}
+      </DropDownContainer>
+    </CourseMain>
+  );
+}
+
+export default CourseRequirements
