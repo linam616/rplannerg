0fae94aeaa3d79177cdcd4a5b26bdeec9dea1969
Lina Mei <linamei0616@gmail.com>
2022-05-14 14:02:15 -0500

    future goals

--- Project/rplannerg/src/App.js
+++ Project/rplannerg/src/App.js
@@ -4,12 +4,16 @@ import CourseRequirements from './components/CourseRequirements';
 import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <div className='Main'>
-      <TodoList />
-      <CourseRequirements />
-    </div>
-  );
+    return ( <
+        div className = 'Main' > <
+        TodoList / >
+        <
+        CourseRequirements / >
+        <
+        /div>
+    );
 }
+// next step should be adding more columns
+// future steps : adding point system + database
 
 export default App;
